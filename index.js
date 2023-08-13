1a76c21021cd347682298031fc127f5a342c3490
masaiharshit <harshit007.ara@gmail.com>
2023-08-13 23:47:45 +0530

    auth

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const app=express();
+
+
+app.get("/",(req,res)=>{
+    res.send("Home Page")
+})
+
+
+
+
+
+
+
+
+app.listen(4000,()=>{
+   console.log("server is running");
+})
